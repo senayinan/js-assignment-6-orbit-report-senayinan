0d491cf653f8b456fdb90554b71db914c73ecc17
Senay Inan <senayinan@Senays-MacBook-Pro.local>
2023-03-17 12:06:17 -0500

    Part 4 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,10 +41,12 @@ export class AppComponent {
 		let matchingSatellites: Satellite[] = [];
 		searchTerm = searchTerm.toLowerCase();
 		for(let i=0; i < this.sourceList.length; i++) {
+
 			let name = this.sourceList[i].name.toLowerCase();
 			if (name.indexOf(searchTerm) >= 0) {
 				matchingSatellites.push(this.sourceList[i]);
 			}
+			
 		}
 		// assign this.displayList to be the array of matching satellites
 		// this will cause Angular to re-make the table, but now only containing matches
